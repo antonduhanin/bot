cfdf6e32edfc5e9550be31178aef0059ef775f29
anton.dukhanin <antonduhanin4444@gmail.com>
2019-02-19 00:39:36 +0300

    add new case

--- bot.py
+++ bot.py
@@ -29,21 +29,20 @@ class BotHandler:
     def get_last_update(self):
         get_result = self.get_updates()
 
-
-
         if len(get_result) > 0:
             last_update = get_result[-1]
         else:
             last_update = get_result[len(get_result)]
 
-        if('message' in last_update and self.last_update_id==0):
+        if ('message' in last_update and self.last_update_id == 0):
             self.last_update_id = last_update['message']['message_id']
             return ''
-        elif('message' in last_update and self.last_update_id != last_update['message']['message_id']):
-            self.last_update_id=last_update['message']['message_id']
+        elif ('message' in last_update and self.last_update_id != last_update['message']['message_id']):
+            self.last_update_id = last_update['message']['message_id']
             return last_update
         else:
             return ''
+
 
 greet_bot = BotHandler('659162348:AAHI4kbFbxslyLZD5GsjdBDl-_3aqQ0Wbt4')
 
@@ -52,7 +51,7 @@ def main():
     now = datetime.datetime.now()
 
     hour = now.hour
-    count = 0
+    count = 2
     while True:
         greet_bot.get_updates()
 
@@ -62,16 +61,18 @@ def main():
         if last_update != '':
             last_chat_text = last_update['message']['text']
             last_chat_id = last_update['message']['chat']['id']
+            name = ['message']['from']['first_name']
 
             if last_chat_text == '/petuh' and hour != now.hour:
-                count+=1
-                s = str("Oleg petuh of the day : "+str(count)+" time")
-                greet_bot.send_message(last_chat_id,s)
+                count += 1
+                s = str("Oleg petuh of the day : " + str(count) + " time")
+                greet_bot.send_message(last_chat_id, s)
                 hour = now.hour
             elif last_chat_text == '/petuh':
-                greet_bot.send_message(last_chat_id,'try later please')
-                greet_bot.send_message(last_chat_id,'What a moron!')
-
+                greet_bot.send_message(last_chat_id, 'try later please')
+                greet_bot.send_message(last_chat_id, 'What a moron!')
+            elif name == 'Oleg':
+                greet_bot.send_message(last_chat_id, 'Петухам' + str(count) + '-разовым слово не давали')
 
 
 if __name__ == '__main__':
